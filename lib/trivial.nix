cf7c377ee2429b513e7ce8b89120cd0486806dde
Mathnerd314 <mathnerd314.gph+hs@gmail.com>
2015-10-19 09:11:12 -0600

    all-packages: Change packageOverrides so that they are applied package-by-package
    Implements recursive merging, and clean-up overridePkgs.

--- lib/trivial.nix
+++ lib/trivial.nix
@@ -13,7 +13,15 @@ rec {
   mergeAttrs = x: y: x // y;
 
   # Take a function and evaluate it with its own returned value.
-  fix = f: let result = f result; in result;
+  fix = f: let x = f x; in x;
+
+  # Like fix but with a way to escape the recursion
+  fix' = f: let x = f x // { __unfix__ = f; }; in x;
+
+  # Given a fix-able function and an overriding
+  # function, this applies the overrides and
+  # returns another fix-able function
+  extend = rattrs: f: self: let super = rattrs self; in super // f self super;
 
   # Flip the order of the arguments of a binary function.
   flip = f: a: b: f b a;
